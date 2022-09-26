577f869232e1a8c887297c11673537c708d22419
RubensStrassi <rubensstrassi@gmail.com>
2022-09-26 16:59:38 -0300

    Criado tela de login e de Cadastro

---
+++ src/app/home/login/login.component.ts
@@ -0,0 +1,31 @@
+import { AutenticacaoService } from './../../autenticacao/autenticacao.service';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  usuario = '';
+  senha = '';
+
+  constructor(private authService: AutenticacaoService, private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  login() {
+    console.log(this.usuario)
+    console.log(this.senha)
+    this.authService.autenticar(this.usuario, this.senha)
+      .subscribe(() => {
+        console.log("Sucesso")
+        this.router.navigate(['principal'])
+      }, error => {
+        console.log("Deu ruim")
+      })
+  }
+
+}
